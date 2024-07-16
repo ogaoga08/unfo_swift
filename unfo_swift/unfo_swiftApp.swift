230f76a3db35a9e39e9c0b0af679218f4ede753a
ogaoga08 <kamekameshin0514@gmail.com>
2024-07-17 01:40:18 +0900

    Initial Commit

---
+++ unfo_swift/unfo_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  unfo_swiftApp.swift
+//  unfo_swift
+//
+//  Created by 小笠原慎 on 2024/07/17.
+//
+
+import SwiftUI
+
+@main
+struct unfo_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
